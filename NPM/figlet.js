f57aa5501c1d70a9cec4ac212a21549737cd0086
Abbaskazi <kaziabbasce09@gmail.com>
2023-12-25 14:00:21 +0530

    Second Changes

--- NPM/figlet.js
+++ NPM/figlet.js
@@ -1,6 +1,6 @@
 var figlet = require("figlet");
 
-figlet("Hello World!!", function (err, data) {
+figlet("Abbas is Hacker!!", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
